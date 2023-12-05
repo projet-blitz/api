18d9799b4381ce5fefee2d48abfe07441179366d
cm\cam <test>
2023-12-05 12:18:27 -0500

    Removed swagger

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddHostedService<GtfsStaticUpdateService>();
 
@@ -20,11 +17,7 @@ GlobalStore.GlobalVar[UpdateFlagKey] = false;
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment()) { }
 
 app.UseStatusCodePages();
 
